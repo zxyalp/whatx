c732fae66cc6d6d62ed2c0c014b24f2c2d1de742
xing yan <zgyan012@gmail.com>
2017-12-07 19:06:26 +0800

    add header

--- src/main/java/com/tmsx/noclient/base/DefaultHeaders.java
+++ src/main/java/com/tmsx/noclient/base/DefaultHeaders.java
@@ -4,6 +4,7 @@ import org.apache.http.Header;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by yang.zhou on 2017/12/6.
@@ -11,5 +12,6 @@ import java.util.List;
 public class DefaultHeaders {
 
     private Header[] headers;
+    private Map<String, List<String>> headerMap;
 
 }
